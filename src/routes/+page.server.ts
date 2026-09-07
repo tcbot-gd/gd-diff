57f9fb4e5716320de8831e1785dd35048a4817d2
chagh <contact@tcbot.pro>
2026-09-07 20:15:18 +0300

    Home page: show only versions that actually have binaries

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,14 @@ import type { PageServerLoad } from './$types';
 import { listPlatforms, listVersions, versionPlatformMatrix } from '$lib/server/repo';
 
 export const load: PageServerLoad = () => {
+	const platforms = listPlatforms();
+	const matrix = versionPlatformMatrix();
+	const versions = listVersions().filter(
+		(version) => Object.keys(matrix[version.id] ?? {}).length > 0
+	);
 	return {
-		platforms: listPlatforms(),
-		versions: listVersions(),
-		matrix: versionPlatformMatrix()
+		platforms,
+		versions,
+		matrix
 	};
 };
